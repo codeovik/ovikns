a25dfa3cb314ce70da2841d9575c8320a976dd57
Ovik <amiobiswas0@gmail.com>
2025-12-21 22:20:20 +0600

    fuc

--- backend/app.js
+++ backend/app.js
@@ -32,3 +32,5 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running on http://localhost:${process.env.PORT} from ${new Date().toLocaleDateString("en-GB")} at ${new Date().toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true })}`)
 })
+
+export default app;
